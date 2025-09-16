6f01408dabd100db397f8d7afc54dcf5ccc587b1
Ethan Robert <ethan.rbrt90@gmail.com>
2025-09-16 10:59:58 +0200

    Fixed minor things

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,10 +1,11 @@
 window.addEventListener('scroll', () => {
     const nav = document.getElementsByTagName('nav')[0];
     const scrollThreshold = 50; // Change this value to adjust scroll trigger point
-
     if (window.scrollY > scrollThreshold) {
         nav.classList.add('nav-free');
     } else {
         nav.classList.remove('nav-free');
     }
+		
+	
 });
